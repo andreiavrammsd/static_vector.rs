24d28222737a920262dfb363a86d9f4bdc061a71
Andrei Avram <6795248+andreiavrammsd@users.noreply.github.com>
2025-05-21 11:38:57 +0300

    Generate all

--- .github/doc/doc.py
+++ .github/doc/doc.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
-import subprocess, os, re, shutil
+import argparse
+import os
+import re
+import shutil
+import subprocess
 
 
 def get_git_versions():
@@ -27,39 +31,66 @@ def render_versions(template_path, versions):
 
     vars = []
     for version in versions:
-        var = {
+        vars.append({
             "version": version,
-        }
-        vars.append(var)
+        })
 
     return "".join(template.format(**var) for var in vars)
 
 
 SRC_DIR = ".github/doc"
 DOC_DIR = "target/doc"
-RENDER_DIR = "target/doc-rendered"
+RENDER_DIR = "target/rendered-doc"
 
 
-def generate(args):
+def generate_version(version):
     subprocess.run(["cargo", "doc", "--no-deps"], check=True)
 
-    version_dst = f"{RENDER_DIR}/{args.version}"
-    os.makedirs(version_dst, exist_ok=True)
-
-    shutil.rmtree(version_dst)
-    shutil.copytree(DOC_DIR, version_dst)
-    shutil.copy(f"{SRC_DIR}/index_redirect.html", f"{version_dst}/index.html")
+    version_dst = f"{RENDER_DIR}/{version}"
+    shutil.copytree(DOC_DIR, version_dst, dirs_exist_ok=True)
 
 
-def index(args):
-    os.makedirs(DOC_DIR, exist_ok=True)
+def switch_branch(branch):
+    subprocess.run(["git", "checkout", branch], check=True)
 
-    html_output = render_index(f"{SRC_DIR}/index_template.html", ["latest"] + get_git_versions())
+
+def index(versions):
+    os.makedirs(RENDER_DIR, exist_ok=True)
+
+    html_output = render_index(f"{SRC_DIR}/index_template.html", versions)
 
     with open(f"{RENDER_DIR}/index.html", "w", encoding="utf-8") as f:
         f.write(html_output)
 
-import argparse
+
+def create_version_dirs(version):
+    version_dst = f"{RENDER_DIR}/{version}"
+    os.makedirs(version_dst, exist_ok=True)
+    shutil.copy(f"{SRC_DIR}/index_redirect.html", f"{version_dst}/index.html")
+
+
+def generate(args):
+    versions = get_git_versions()
+
+    for version in versions:
+        print(version)
+
+    shutil.rmtree(RENDER_DIR, ignore_errors=True)
+    create_version_dirs("latest")
+    generate_version("latest")
+
+
+    for version in versions:
+        create_version_dirs(version)
+
+    for version in versions:
+        switch_branch(version)
+        generate_version(version)
+
+    switch_branch(args.branch)
+
+    index(["latest"] + versions)
+
 
 def main():
     parser = argparse.ArgumentParser(description="Documentation")
@@ -67,15 +98,12 @@ def main():
 
     # `generate` command
     generate_parser = subparsers.add_parser("generate", help="Generate documentation")
-    generate_parser.add_argument("--version", required=True, help="Version to generate")
+    generate_parser.add_argument("--branch", required=True, help="Starting branch")
     generate_parser.set_defaults(func=generate)
-
-    # `index` command
-    index_parser = subparsers.add_parser("index", help="Generate index.html")
-    index_parser.set_defaults(func=index)
 
     args = parser.parse_args()
     args.func(args)
+
 
 if __name__ == "__main__":
     main()
