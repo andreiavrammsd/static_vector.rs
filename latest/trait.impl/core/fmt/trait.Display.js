ce70f818adc14fd835d6ae16d53421b8460d7fa9
andreiavrammsd <andreiavrammsd@users.noreply.github.com>
2025-05-22 13:51:26 +0000

    deploy: af1e0158161c6497f24038180df3d199c95e8846

--- latest/trait.impl/core/fmt/trait.Display.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["static_vector",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"static_vector/struct.CapacityExceededError.html\" title=\"struct static_vector::CapacityExceededError\">CapacityExceededError</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"static_vector/struct.LengthTooLargeError.html\" title=\"struct static_vector::LengthTooLargeError\">LengthTooLargeError</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[625]}
--- v0.1.0/trait.impl/core/ops/drop/trait.Drop.js
+++ v0.1.0/trait.impl/core/ops/drop/trait.Drop.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["static_vector",[["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a>, const CAPACITY: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.87.0/core/primitive.usize.html\">usize</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"static_vector/struct.Vec.html\" title=\"struct static_vector::Vec\">Vec</a>&lt;T, CAPACITY&gt;"]]]]);
+    var implementors = Object.fromEntries([["static_vector",[["impl&lt;T, const CAPACITY: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.87.0/core/primitive.usize.html\">usize</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"static_vector/struct.Vec.html\" title=\"struct static_vector::Vec\">Vec</a>&lt;T, CAPACITY&gt;"]]],["static_vector",[["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a>, const CAPACITY: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.87.0/core/primitive.usize.html\">usize</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"static_vector/struct.Vec.html\" title=\"struct static_vector::Vec\">Vec</a>&lt;T, CAPACITY&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[554]}
+//{"start":57,"fragment_lengths":[419,555]}
