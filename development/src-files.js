ce3de2c3a5ad70bc30a72a22d921f8b610689f0b
Andrei Avram <6795248+andreiavrammsd@users.noreply.github.com>
2025-05-22 22:33:15 +0300

    Remove development

--- development/src-files.js
+++
@@ -1,3 +0,0 @@
-var srcIndex = new Map(JSON.parse('[["static_vector",["",[],["lib.rs"]]]]'));
-createSrcSidebar();
-//{"start":36,"fragment_lengths":[36]}
--- development/static_vector/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"struct":["CapacityError","Iter","IterMut","Vec"]};
