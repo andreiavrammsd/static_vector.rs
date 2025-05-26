2516fbc3ed1593c7b0e1c7f34032aa2438395442
andreiavrammsd <andreiavrammsd@users.noreply.github.com>
2025-05-26 15:23:26 +0000

    deploy: f7641fce2bb97235a340208e6b74c0f3abda7f8f

--- master/trait.impl/core/fmt/trait.Debug.js
+++ master/trait.impl/core/fmt/trait.Debug.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["static_vector",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"static_vector/struct.CapacityError.html\" title=\"struct static_vector::CapacityError\">CapacityError</a>"]]]]);
+    var implementors = Object.fromEntries([["static_vector",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"static_vector/struct.CapacityError.html\" title=\"struct static_vector::CapacityError\">CapacityError</a>"],["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a>, const CAPACITY: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.87.0/core/primitive.usize.html\">usize</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"static_vector/struct.Vec.html\" title=\"struct static_vector::Vec\">Vec</a>&lt;T, CAPACITY&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[295]}
+//{"start":57,"fragment_lengths":[818]}
