26842d6adce7dbe730fbd6413ef3551ae4cd722a
Andrei Avram <6795248+andreiavrammsd@users.noreply.github.com>
2025-05-27 06:40:29 +0300

    Create vector with capacity and elements

--- src/macros.rs
+++ src/macros.rs
@@ -4,10 +4,12 @@
 ///
 /// - `vec![T; CAPACITY]`: Creates a new empty `Vec<T, CAPACITY>` with maximum `CAPACITY` elements of type `T`.
 /// - `vec![x, y, z]`: Creates a `Vec` initialized with the given values. The capacity is inferred from the number of elements.
+/// - `vec![CAPACITY; x, y, z]`: Creates a `Vec` with given `CAPACITY`, initialized with the given values.
 ///
 /// # Panics
 ///
-/// Panics if `CAPACITY == 0`. Zero-capacity vectors are not supported.
+/// - If `CAPACITY == 0`. Zero-capacity vectors are not supported.
+/// - If the number of of elements used to initialize the vector is larger than the given `CAPACITY`.
 ///
 /// # Examples
 ///
@@ -24,6 +26,12 @@
 /// let vec = static_vector::vec![1, 2, 3];
 /// assert_eq!(vec.as_slice(), &[1, 2, 3]);
 /// ```
+///
+/// Create a vector with a specific capacity and elements:
+/// ```rust
+/// let vec = static_vector::vec![10; 1, 2, 3];
+/// assert_eq!(vec.as_slice(), &[1, 2, 3]);
+/// ```
 #[macro_export]
 macro_rules! vec {
     ($type:ty; $capacity:literal) => {
@@ -33,9 +41,24 @@ macro_rules! vec {
     ($($value:expr),+ $(,)?) => {
         {
             let mut vec = $crate::Vec::<_, { [$($value),+].len() }>::new();
-            // It's safe to call unwrap because we are initializing the vector with a known number of elements
+            // It's safe to call expect because we are initializing the vector with a known number of elements
             // (which is also the capacity).
-            vec.extend_from_slice(&[$($value),+]).unwrap();
+            vec.extend_from_slice(&[$($value),+]).expect("length matches capacity");
+            vec
+        }
+    };
+
+    ($capacity:literal; $($value:expr),+ $(,)?) => {
+        {
+            assert!(
+                $capacity >= { [$($value),+].len() },
+                "too many elements ({}) for CAPACITY ({})", { [$($value),+].len() }, $capacity
+            );
+
+            let mut vec = $crate::Vec::<_, $capacity>::new();
+            // It's safe to call expect because we are initializing the vector with a known number of elements
+            // (which is less than or equal to the capacity).
+            vec.extend_from_slice(&[$($value),+]).expect("length matches capacity");
             vec
         }
     };
@@ -62,5 +85,19 @@ mod tests {
         assert_eq!(vec.capacity(), 3);
         assert_eq!(vec.len(), 3);
         assert_eq!(vec.as_slice(), &[1, 2, 3]);
+    }
+
+    #[test]
+    fn vec_with_capacity_and_elements() {
+        let vec = vec![10; 1, 2, 3];
+        assert_eq!(vec.capacity(), 10);
+        assert_eq!(vec.len(), 3);
+        assert_eq!(vec.as_slice(), &[1, 2, 3]);
+    }
+
+    #[test]
+    #[should_panic(expected = "too many elements (3) for CAPACITY (2)")]
+    fn vec_with_more_elements_than_capacity() {
+        let _ = vec![2; 1, 2, 3];
     }
 }
